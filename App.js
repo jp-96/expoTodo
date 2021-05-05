6327a272803a9ebea526480c6b24751c154eb6b3
cf-sx4 <cf-sx4@example.com>
2021-05-05 13:28:12 +0900

    4-7

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar as ReactNStatubBar, Platform } from 'react-native';
+import { StyleSheet, Text, View, StatusBar as ReactNStatubBar, Platform,
+         ScrollView,
+} from 'react-native';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : ReactNStatubBar.currentHeight;
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.filter}>
+          <Text>フィルターがここに配置される</Text>
+        </View>
+        <ScrollView style={styles.todolist}>
+          <Text>ToDoリストがここに配置される</Text>
+        </ScrollView>
+        <View style={styles.input}>
+          <Text>テキスト入力がここに配置される</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -20,5 +31,14 @@ const styles = StyleSheet.create({
     paddingTop: STATUSBAR_HEIGHT,
     // alignItems: 'center',
     // justifyContent: 'center',
+  },
+  filter: {
+    height: 30,
+  },
+  todolist: {
+    flex: 1,
+  },
+  input: {
+    height: 30,
   },
 });
